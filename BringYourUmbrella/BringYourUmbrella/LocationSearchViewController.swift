e7d12d4c6b06ebf895fc8a4b87f8c8216ca752d4
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-05-13 18:02:26 +0900

    라이브러리 추가

---
+++ BringYourUmbrella/BringYourUmbrella/LocationSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LocationSearchViewController.swift
+//  BringYourUmbrella
+//
+//  Created by 희라 on 5/13/24.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
